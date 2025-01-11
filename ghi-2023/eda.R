e44343824ea7e1ff56b8200ee26820ac797a8e38
Noah Carlos <ndcarlos415@gmail.com>
2025-01-11 17:52:16 +0700

    eda cleanup

--- ghi-2023/eda.R
+++ ghi-2023/eda.R
@@ -5,21 +5,5 @@ library(config)
 library(maps)
 library(rworldmap)
 library(sf)
-
-world_map_sf <- st_as_sf(world_map)
-
-ghi_23 <- dat[dat$year == 2023, ]
-
-world_map_sf <- merge(world_map_sf, ghi_23, by.x = 'ISO3', by.y = 'country_code')
-
-# need to fix this plot
-# issues with colorfill
-ggplot(data = world_map_sf) +
-  geom_sf(aes(fill = 'life_expect')) +
-  scale_fill_viridis_d(option = 'plasma', na.value = 'grey90') +
-  theme_minimal() +
-  labs(title = 'Map of 2023 Life Expectancies by Countries',  fill = 'Life Expectancy')
-
-
-
+library(dplyr)
 
