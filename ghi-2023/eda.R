c1fb503dc50cd5cb39d6627553c3bd03dea26f4e
Noah Carlos <ndcarlos415@gmail.com>
2025-01-16 19:29:10 -0800

    restart eda and clear file

--- ghi-2023/eda.R
+++ ghi-2023/eda.R
@@ -2,8 +2,20 @@
 
 library(ggplot2)
 library(config)
-library(maps)
-library(rworldmap)
-library(sf)
 library(dplyr)
+
+dat <- read.csv('/Users/home/Documents/Projects/R/ghi-2023/ghi-2023/data/world_health_data.csv')
+
+summary(dat)
+
+
+eda_subset<- c('BRA', 'GRC', 'JPN', 'KOR', 'MAR',  'PRT', 'THA', 'TZA', 'USA', 'VNM')
+
+dat_eda <- dat %>%
+  filter(country_code %in% eda_subset)
+
+head(dat_eda)
+  
+
+
 
