59c3bf14d7e65141bd84eb257e9f47e37d5b66d1
Noah Carlos <ndcarlos415@gmail.com>
2025-01-21 18:45:10 -0800

    Life Expectancy plot eda

--- ghi-2023/eda.R
+++ ghi-2023/eda.R
@@ -2,20 +2,11 @@
 
 library(ggplot2)
 library(config)
+library(maps)
+library(rworldmap)
+library(sf)
 library(dplyr)
 
-dat <- read.csv('/Users/home/Documents/Projects/R/ghi-2023/ghi-2023/data/world_health_data.csv')
+head(dat)
 
 summary(dat)
-
-
-eda_subset<- c('BRA', 'GRC', 'JPN', 'KOR', 'MAR',  'PRT', 'THA', 'TZA', 'USA', 'VNM')
-
-dat_eda <- dat %>%
-  filter(country_code %in% eda_subset)
-
-head(dat_eda)
-  
-
-
-
