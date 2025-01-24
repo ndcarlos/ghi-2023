44d3837094c5b25c11f6f6ef428656aa930d64c5
Noah Carlos <ndcarlos415@gmail.com>
2025-01-23 16:53:57 -0800

    sync with local

--- ghi-2023/eda.R
+++ ghi-2023/eda.R
@@ -1,12 +1,34 @@
 # Exploratory Data Analysis
 
 library(ggplot2)
-library(config)
-library(maps)
-library(rworldmap)
-library(sf)
 library(dplyr)
+library(patchwork)
 
 head(dat)
 
-summary(dat)
+eda_subset <- c("BRA", "GRC", "JPN", "KOR", "MAR", "PRT", "THA", "TZA", "USA", "VNM")
+
+eda_df <- dat %>%
+  filter(country_code %in% eda_subset) %>%
+  sort(life_expect)
+
+plot1 <- ggplot(eda_df, aes(x = year, y = life_expect, color = country_code)) +
+  geom_line(size = 0.75) +
+  labs(
+    title = "Life Expectancy Over Time",
+    x = "Year",
+    y = "Life Expectancy"
+  ) +
+  theme_minimal()
+
+plot2 <- ggplot(eda_df, aes(x = year, y = infant_mortality, color = country_code)) +
+  geom_line(size = 0.75) +
+  labs(
+    title = "Infant Mortality Over Time",
+    x = "Year",
+    y = "Infant Mortality"
+  ) +
+  theme_minimal()
+
+# Combine plots side by side
+plot1 + plot2
