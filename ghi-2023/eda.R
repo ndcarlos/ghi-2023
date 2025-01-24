fdb2dbfc9224278c35b03bea990a06686e5f99c4
Noah Carlos <ndcarlos415@gmail.com>
2025-01-24 06:40:26 -0800

    sync

--- ghi-2023/eda.R
+++ ghi-2023/eda.R
@@ -10,7 +10,7 @@ eda_subset <- c("BRA", "GRC", "JPN", "KOR", "MAR", "PRT", "THA", "TZA", "USA", "
 
 eda_df <- dat %>%
   filter(country_code %in% eda_subset) %>%
-  sort(life_expect)
+  arrange(life_expect, descending = TRUE)
 
 plot1 <- ggplot(eda_df, aes(x = year, y = life_expect, color = country_code)) +
   geom_line(size = 0.75) +
@@ -30,5 +30,32 @@ plot2 <- ggplot(eda_df, aes(x = year, y = infant_mortality, color = country_code
   ) +
   theme_minimal()
 
+plot3 <- ggplot(eda_df, aes(x = year, y = neonatal_mortality, color = country_code)) +
+  geom_line(size = 0.75) +
+  labs(
+    title = "Neonatal Over Time",
+    x = "Year",
+    y = "Neonatal Mortality"
+  ) +
+  theme_minimal()
+
+plot4 <- ggplot(eda_df, aes(x = year, y = under_5_mortality, color = country_code)) +
+  geom_line(size = 0.75) +
+  labs(
+    title = "Under 5 Mortality Over Time",
+    x = "Year",
+    y = "Under 5 Mortality"
+  ) +
+  theme_minimal()
+
+plot5 <- ggplot(eda_df, aes(x = year, y = prev_hiv, color = country_code)) +
+  geom_line(size = 0.75) +
+  labs(
+    title = "Previous HIV Over Time",
+    x = "Year",
+    y = "Previous HIV"
+  ) +
+  theme_minimal()
+
 # Combine plots side by side
-plot1 + plot2
+plot1 + plot2 + plot3 + plot4 + plot5
